fa4f57bd82c3cea9cd5a85627e1bb3da01a812bd
Dread <lubko@Skrawmnie>
2023-08-08 19:03:54 +0200

    add Login and protocol registration

--- garage_django/garage/urls.py
+++ garage_django/garage/urls.py
@@ -4,6 +4,11 @@ from . import views
 
 
 urlpatterns = [
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+    path('register/', views.registerPage, name="register"),
+
+
     path('', views.home, name='home'),
     path('repair/<str:pk>/', views.repair, name='repair'),
     path('create-repair/', views.createRepair, name='createrepair'),
--- garage_django/garage/views.py
+++ garage_django/garage/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.db.models import Q
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from .models import Repairs
 from .forms import RepairForm
 
@@ -10,6 +15,50 @@ from .forms import RepairForm
 # ]
 
 # Create your views here.
+
+def loginPage(request):
+    page = 'login'
+    
+    if request.method == 'POST':
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+           messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'User OR password does not exist')         
+    
+    context = {'page': page}
+    return render(request, 'garage/login_register.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
+def registerPage(request):
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occured during registration')
+
+    return render(request, 'garage/login_register.html', {'form': form})
+
 def home(request):
     repairs = Repairs.objects.all()
     return render(request, 'garage/home.html', {'repairs':repairs})
